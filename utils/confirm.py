27d20a4d45ab2402274ad76231d6f027ec09ba17
bertis-prai <91928815+bertis-prai@users.noreply.github.com>
2021-11-01 17:17:35 +0900

    Update confirm.py

--- utils/confirm.py
+++ utils/confirm.py
@@ -1,10 +1,9 @@
 import os
-import torch
 
 def get_initial_checkpoint(config, new_check_dir, name=None):
     checkpoint_dir = os.path.join("./logs", config.RECIPE_DIR, new_check_dir)
     if name:
-        return os.path.join(checkpoint_dir, name+'.pth')
+        return os.path.join(checkpoint_dir, name)
     else:
         checkpoints = [checkpoint
                        for checkpoint in os.listdir(checkpoint_dir) if checkpoint.endswith('.pth')]
@@ -13,13 +12,3 @@ def get_initial_checkpoint(config, new_check_dir, name=None):
             print(list(sorted(checkpoints))[0])
             return os.path.join(checkpoint_dir, list(sorted(checkpoints))[0])
     return None
-
-def pearsonr(x, y):
-    mean_x = torch.mean(x)
-    mean_y = torch.mean(y)
-    xm = x.sub(mean_x)
-    ym = y.sub(mean_y)
-    r_num = xm.dot(ym)
-    r_den = torch.norm(xm, 2) * torch.norm(ym, 2)
-    r_val = r_num / r_den
-    return r_val
